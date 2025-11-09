732785960613e85a2a65bb6a50661cff16ea0369
jennimotaa <carriejennifferalves@gmail.com>
2025-11-09 07:48:59 -0300

    Add comments and print statements for BSM phrases
    Added introductory comments and a print statement to display collaborative phrases about BSMs.

--- frases.py
+++ frases.py
@@ -1,1 +1,6 @@
 # projeto_gen
+# vamos criar frases para um texto sobre BSMs
+
+print("Vamos ver seis frases colaborativas que descrevem sobre BSMs")
+
+print ("1 - A autoavaliação diagnóstica de BSMs no início do programa serve como ponto de partida para medir e desenvolver as soft skills dos participantes.")
