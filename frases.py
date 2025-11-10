1a43c9a5eb9d51d2a752dc19e5f8a84060d32a37
karinamsilva <karinamartinssilva@live.com>
2025-11-10 18:36:18 -0300

    adding avaliações técnico formativas

--- frases.py
+++ frases.py
@@ -4,3 +4,6 @@
 print("Vamos ver seis frases colaborativas que descrevem sobre BSMs")
 
 print ("1 - A autoavaliação diagnóstica de BSMs no início do programa serve como ponto de partida para medir e desenvolver as soft skills dos participantes.")
+
+print('''\n4 - Avaliações técnico formativas buscam medir aprendizado técnico e acompanhar o processo de desenvolvimento, servindo como ferramenta 
+para mudar de rota com base nas informações coletadas. Muito mais que comprovar o conhecimento, é entender a curva de aprendizado \n''')
