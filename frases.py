af9c653835044fc9a4ebac522ae1d6745cdaa828
Caroline Henrique <carolhenriq2@gmail.com>
2025-11-11 20:18:30 -0300

    Subindo frase 2

--- frases.py
+++ frases.py
@@ -5,6 +5,8 @@ print("Vamos ver seis frases colaborativas que descrevem sobre BSMs")
 
 print ("1 - A autoavaliação diagnóstica de BSMs no início do programa serve como ponto de partida para medir e desenvolver as soft skills dos participantes.")
 
+print("2 - A avaliação formativa de BSM mede o progresso das habilidades socioemocionais ao longo do programa por meio de autorreflexão, avaliação em gruppo e dramatizações")
+
 print('''\n4 - Avaliações técnico formativas buscam medir aprendizado técnico e acompanhar o processo de desenvolvimento, servindo como ferramenta 
 para mudar de rota com base nas informações coletadas. Muito mais que comprovar o conhecimento, é entender a curva de aprendizado \n''')
 
