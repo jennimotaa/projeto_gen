4a69aed6d1609b77ab87e8752dff60811e97749c
knolexdev <thgjacques@gmail.com>
2025-11-10 21:43:25 -0300

    Adicionando a frase 5

--- frases.py
+++ frases.py
@@ -4,3 +4,5 @@
 print("Vamos ver seis frases colaborativas que descrevem sobre BSMs")
 
 print ("1 - A autoavaliação diagnóstica de BSMs no início do programa serve como ponto de partida para medir e desenvolver as soft skills dos participantes.")
+
+print ('5 - A avaliação da técnica somativa serve para verificar, ao final de um ciclo de aprendizagem, o quanto os participantes dominaram os conhecimentos e habilidades trabalhados.')
