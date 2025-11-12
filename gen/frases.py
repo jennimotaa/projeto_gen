ca1e571b77381598fde6c2a9b3e59711679cd3f4
nara <narasantos.tech@gmail.com>
2025-11-12 00:13:39 -0300

    Adiciona frase da Nara

---
+++ gen/frases.py
@@ -0,0 +1,4 @@
+print("""
+Colocando na balança todo o progresso, as avaliações técnicas somativas representam o momento
+de medir, com clareza, o quanto evoluímos em conhecimento e prática ao longo da jornada de aprendizagem.
+""")
