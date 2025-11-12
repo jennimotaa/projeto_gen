7760f3122a7ad56f144b5f30772d4324e91cf5f0
nahcodee <narasantos.tech@gmail.com>
2025-11-12 09:31:28 -0300

    Remove commented-out text from frases.py
    Remove commented-out block of text in frases.py

--- gen/frases.py
+++ gen/frases.py
@@ -1,4 +1,1 @@
-print("""
-Colocando na balança todo o progresso, as avaliações técnicas somativas representam o momento
-de medir, com clareza, o quanto evoluímos em conhecimento e prática ao longo da jornada de aprendizagem.
-""")
+
